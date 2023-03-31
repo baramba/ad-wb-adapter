0d9b54503a82beca567ee3ddbf1a063f5014d333
Ostap Kormiltsev <error1number404@gmail.com>
2023-03-31 23:31:53 +0700

    AD-61-feature-refactoring

--- app/src/depends/arq.py
+++ app/src/depends/arq.py
@@ -5,5 +5,9 @@ from core.settings import settings
 
 
 async def get_arq() -> ArqRedis:
-    return await create_pool(RedisSettings(host=settings.REDIS.HOST,
-                                           port=settings.REDIS.PORT))
+    return await create_pool(
+        RedisSettings(
+            host=settings.REDIS.HOST,
+            port=settings.REDIS.PORT
+        ),
+    )
--- app/src/utils.py
+++ app/src/utils.py
@@ -1,6 +1,9 @@
+import datetime as dt
 import inspect
 from functools import wraps
 from typing import Callable, Awaitable, Any, Union, Iterable
+
+from arq import ArqRedis, Retry
 
 
 def depends_decorator(
@@ -25,6 +28,33 @@ def depends_decorator(
                     result = await result
                 inner_kwargs[key] = result
             return await func(*args, **kwargs, **inner_kwargs)
+
+        return inner
+
+    return func_wrapper
+
+
+async def run_kinda_async_task(arq_poll: ArqRedis, *args, **kwargs) -> Any:
+    job = await arq_poll.enqueue_job(
+        *args,
+        **kwargs
+    )
+    return await job.result()
+
+
+def retry_(
+        defer_: Union[dt.timedelta, int] = dt.timedelta(seconds=1),
+        max_tries: int = 5,
+):
+    def func_wrapper(func):
+        @wraps(func)
+        async def inner(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                if args[0]['job_try'] == max_tries:
+                    raise e from e
+                raise Retry(defer=defer_) from e
 
         return inner
 
